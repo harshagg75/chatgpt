4e85a7a6ece284a4f8ed8686f8a8c1f817d13553
ADMIN <harshagg75@gmail.com>
2023-04-27 15:15:19 +0530

    create a gpt chatbot

---
+++ backend.py
@@ -0,0 +1,21 @@
+import openai
+
+
+class ChatBox:
+    def __init__(self):
+        openai.api_key = "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"
+
+    def get_response(self, user_input):
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=user_input,
+            max_tokens=3000,
+            temperature=0.5
+        ).choices[0].text
+        return response
+
+
+if __name__ == "__main__":
+    chatbox = ChatBox()
+    response = chatbox.get_response("write joke on brother.")
+    print(response)
